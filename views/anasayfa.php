0b973b119c026875569f934586ef25138c7a027f
Recep UNCU <recepuncu@gmail.com>
2016-12-03 13:01:07 +0300

    initialize

---
+++ views/anasayfa.php
@@ -0,0 +1,61 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-8 p-r-0">
+                            <ul class="bxslider bxslider-custom">
+                                <li><img src="assets/img/slide1.jpg" width="800" height="350" title="Slide 1" /></li>
+                                <li><img src="assets/img/slide2.JPG" width="800" height="350"  title="Slide 2" /></li>
+                                <li><img src="assets/img/slide3.jpg" width="800" height="350"  title="Slide 3" /></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4">
+                            <ul class="sag-menu">
+                                <li style="background-color:#F44336"> <a href="javascript:;">E-Birlik</a> </li>
+                                <li style="background-color:#E91E63"> <a href="javascript:;">Üye Bilgi Sistemi</a> </li>
+                                <li style="background-color:#9C27B0"> <a href="javascript:;">İnsan Kaynakları</a> </li>
+                                <li style="background-color:#3F51B5"> <a href="javascript:;">Türmob</a> </li>
+                                <li style="background-color:#2196F3"> <a href="javascript:;">Tesmer</a> </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row alt-menu">
+                        <div class="c col-lg-2 p-l-15">
+                            <a href="javascript:;">
+                                <img src="assets/img/logolar/maliyebakanligi.jpg" alt="" width="100%" height="84px;" />
+                            </a>
+                        </div>
+                        <div class="c col-lg-2">
+                            <a href="javascript:;">
+                                <img src="assets/img/logolar/gib-logo1.jpg" alt="" width="100%" height="84px;" />
+                            </a>
+                        </div>
+                        <div class="c col-lg-2">
+                            <a href="javascript:;">
+                                <img src="assets/img/logolar/SGK_1152x864.jpg" alt="" width="100%" height="84px;" />
+                            </a>
+                        </div>
+                        <div class="c col-lg-2">
+                            <a href="javascript:;">
+                                <img src="assets/img/logolar/resmi-gazete.jpg" alt="" width="100%" height="84px;" />
+                            </a>
+                        </div>
+                        <div class="c col-lg-2">
+                            <a href="javascript:;">
+                                <img src="assets/img/logolar/1471347903033.jpg" alt="" width="100%" height="84px;" />
+                            </a>
+                        </div>
+                        <div class="c col-lg-2 p-r-15">
+                            <a href="javascript:;">
+                                <img src="assets/img/logolar/LUCA_LOGO.jpg" alt="" width="100%" height="84px;" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>                        
+    </div>
+</div>
+
---
+++ views/layout.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Manisa Serbest Muhasebeci Mali Müşavirler Odası">
+        <meta name="keywords" content="Manisa, Serbest Muhasebeci, Mali Müşavir, Mali Müşavirler Odası">
+        <meta name="author" content="Recep Uncu">
+    <title><?php echo $title; ?></title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />    
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/js/jquery.bxslider/jquery.bxslider.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/customize.css" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <nav class="navbar navbar-custom">
+        <div class="container">
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-custom-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Menü</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="assets/img/logo.png" alt="MANİSA SERBEST MUHASEBECİ MALİ MÜŞAVİRLER ODASI"/>
+                    <p class="navbar-brand-text">Manisa Serbest Muhasebeci <br />
+                        Mali Müşavirler Odası <br />                        
+                        <span class="sub-brand-text">Chamber of Certified Public Accountants</span>
+                    </p>                    
+                </a>                
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-custom-collapse-1">
+                
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+    <nav class="navbar navbar-default navbar-sub-custom">
+        <div class="container">
+
+            <div class="collapse navbar-collapse" id="navbar-sub-custom-collapse-1">
+                <ul class="nav navbar-nav navbar-left">
+                    <li class="active"><a href="#">ANASAYFA</a></li>                    
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">KURUMSAL <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Tarihçemiz</a></li>
+                            <li><a href="#">Organizasyon Şeması</a></li>
+                            <li><a href="#">Komisyonlar</a></li>
+                            <li><a href="#">Oda Danışma Meclisi</a></li>
+                            <li><a href="#">Temsilcilikler</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">ÜYELERE ÖZEL</a></li>
+                    <li><a href="#">TESMER</a></li>
+                    <li><a href="#">MEVZUAT</a></li>
+                    <li><a href="#">İLETİŞİM</a></li>
+                </ul>                
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>    
+
+<div id="body_content"><?php echo $body_content; ?></div>    
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.pinBox.min.js" type="text/javascript"></script>
+<script src="assets/js/jquery.bxslider/jquery.bxslider.min.js" type="text/javascript"></script>
+<script src="assets/js/script.js" type="text/javascript"></script>
+</body>
+</html>
